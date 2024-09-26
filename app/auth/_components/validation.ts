97485707639d1fc48c56cf3fab0e3a7353f25f05
Paul Ebert <paul.ebert@code.berlin>
2024-09-26 10:04:35 +0200

    adjust auth zod validation scheme

--- app/auth/_components/validation.ts
+++ app/auth/_components/validation.ts
@@ -1,12 +1,20 @@
-// TODO add messages
-import { z } from "zod";
+import { z } from 'zod'
 const registerFormSchema = z.object({
-  username: z.string().min(4, { message: "Too short" }).max(8),
-  password: z.string().min(4).max(8),
-});
+    username: z
+        .string()
+        .min(4, { message: 'Minimum 4 characters required' })
+        .max(12, { message: 'Maximum 12 characters allowed' }),
+    password: z
+        .string()
+        .min(4, { message: 'Your password needs at least 4 characters' })
+        .max(12, {
+            message: 'Your password cannot be longer than 12 characters',
+        }),
+})
 
 const loginFormSchema = z.object({
-  username: z.string().min(4).max(8),
-  password: z.string().min(4).max(8),
-});
-export { registerFormSchema, loginFormSchema };
+    username: z.string().min(4).max(8),
+    password: z.string().min(4).max(8),
+})
+
+export { registerFormSchema, loginFormSchema }
