34a0bd1bc11166cd940a838326aabf02ccffd604
Paul Ebert <paul.ebert@code.berlin>
2024-09-26 10:04:19 +0200

    clean up auth forms

--- app/auth/_components/loginForm/loginForm.tsx
+++ app/auth/_components/loginForm/loginForm.tsx
@@ -1,84 +1,79 @@
-"use client";
+'use client'
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-import { loginFormSchema } from "../validation";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { loginAction } from "@/actions/auth/login";
-import { useActionState } from "react";
-import { useFormState } from "react-dom";
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/components/ui/form'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import { loginFormSchema } from '../validation'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { loginAction } from '@/actions/auth/login'
+import { useActionState } from 'react'
+import { useFormState } from 'react-dom'
 
 export function LoginForm() {
-  const defaultValues = {
-    username: "",
-    password: "",
-  };
+    const defaultValues = {
+        username: '',
+        password: '',
+    }
 
-  const form = useForm<z.infer<typeof loginFormSchema>>({
-    resolver: zodResolver(loginFormSchema),
-    defaultValues,
-  });
+    const form = useForm<z.infer<typeof loginFormSchema>>({
+        resolver: zodResolver(loginFormSchema),
+        defaultValues,
+    })
 
-  const [state, formAction, pending] = useFormState(loginAction, null);
-  // function onSubmit(values: z.infer<typeof loginFormSchema>) {
-  //   console.log(values);
-  // }
-  //
-  // TODO Ensure proper validation and server side error handling
+    const [state, formAction] = useFormState(loginAction, null)
 
-  return (
-    <div>
-      <Form {...form}>
-        <form
-          // onSubmit={form.handleSubmit(onSubmit)}
-          action={formAction}
-        >
-          <FormField
-            name="username"
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="paulecode" required {...field} />
-                </FormControl>
-                <FormDescription>Your username</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            name="password"
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="********"
-                    type="password"
-                    required
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          {state}
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form>
-    </div>
-  );
+    return (
+        <div>
+            <Form {...form}>
+                <form action={formAction} className="flex flex-col gap-4">
+                    <FormField
+                        name="username"
+                        control={form.control}
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Username</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        placeholder="paulecode"
+                                        required
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        name="password"
+                        control={form.control}
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        placeholder="********"
+                                        type="password"
+                                        required
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    {state}
+                    <Button type="submit">Submit</Button>
+                </form>
+            </Form>
+        </div>
+    )
 }
--- app/auth/_components/registerForm/registerForm.tsx
+++ app/auth/_components/registerForm/registerForm.tsx
@@ -1,87 +1,82 @@
-"use client";
+'use client'
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { useForm, useFormState } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { registerFormSchema } from "../validation";
-import { z } from "zod";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import register from "@/actions/auth/register";
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/components/ui/form'
+import { useForm, useFormState } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { registerFormSchema } from '../validation'
+import { z } from 'zod'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import register from '@/actions/auth/register'
 
 // TODO check case
 
 export function RegisterForm() {
-  const form = useForm<z.infer<typeof registerFormSchema>>({
-    resolver: zodResolver(registerFormSchema),
-    defaultValues: {
-      username: "",
-      password: "",
-    },
-  });
+    const form = useForm<z.infer<typeof registerFormSchema>>({
+        resolver: zodResolver(registerFormSchema),
+        defaultValues: {
+            username: '',
+            password: '',
+        },
+    })
 
-  async function onSubmit(values: z.infer<typeof registerFormSchema>) {
-    console.log(values);
-    const { username, password } = values;
-    try {
-      await register(username, password);
-    } catch (error) {
-      console.log("caught it");
-      form.setError("username", { message: (error as Error).message });
+    async function onSubmit(values: z.infer<typeof registerFormSchema>) {
+        console.log(values)
+        const { username, password } = values
+        try {
+            await register(username, password)
+        } catch (error) {
+            form.setError('username', { message: (error as Error).message })
+        }
     }
-  }
 
-  return (
-    <div>
-      <Form {...form}>
-        <form
-          // action={register}
-          onSubmit={form.handleSubmit(onSubmit)}
-        >
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="paulecode" {...field} />
-                </FormControl>
-                <FormDescription>
-                  This is your username you use to log in
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input placeholder="********" type="password" {...field} />
-                </FormControl>
-                <FormDescription>
-                  4-16 characters
-                  <br />
-                  Ideally use a password manager
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form>
-    </div>
-  );
+    return (
+        <div>
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="flex flex-col gap-4"
+                >
+                    <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Username</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="paulecode" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        placeholder="********"
+                                        type="password"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit">Submit</Button>
+                </form>
+            </Form>
+        </div>
+    )
 }
