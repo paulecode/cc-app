c98a11bda0b0729d04caf65907107ee3fda102e1
Paul Ebert <paul.ebert@code.berlin>
2024-09-26 10:06:07 +0200

    add middleware and verifysession

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,24 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { verifySession } from "./midlewares/verifySession";
 
-export function middleware(request: NextRequest) {
-  console.log("middleware");
-  console.log(request.nextUrl);
+export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith("/home")) {
+    const session = await verifySession();
 
-  if (
-    request.nextUrl.pathname.startsWith("/home") ||
-    request.nextUrl.pathname.startsWith("/auth")
-  ) {
-    // return NextResponse.rewrite(new URL("/auth", request.url));
-    // return NextResponse.redirect(new URL("/auth", request.url));
-
-    if (true) {
-    } else {
+    console.log("session", session);
+    if (!session) {
+      return NextResponse.redirect(new URL("/auth", request.url));
     }
   }
 
   if (request.nextUrl.pathname.startsWith("/auth")) {
+    const session = await verifySession();
+
+    if (session) {
+      return NextResponse.redirect(new URL("/home", request.url));
+    }
   }
-  return NextResponse.redirect(new URL("/", request.url));
 }
 
 export const config = {
