cd38da6892beb8f078b447b62db9626b2bf4877b
Paul Ebert <paul.ebert@code.berlin>
2024-09-30 22:31:04 +0200

    add userid to formdata

--- actions/fileUpload/fileUpload.ts
+++ actions/fileUpload/fileUpload.ts
@@ -4,14 +4,18 @@ import { redirect } from 'next/navigation'
 import { ENV } from '@/lib/env'
 import { midiResultHandler } from '../resultHandler/midiResultHandler'
 import { wavResultHandler } from '../resultHandler/wavResultHandler'
+import { verifySession } from '@/midlewares/verifySession'
 
 export const handleFileUpload = async (formData: FormData) => {
     'use server'
 
     const token = cookies().get('session')?.value || ''
 
+    const userId = await verifySession()
+
     const filename = (formData.get('file') as File).name
     const filetype = (formData.get('file') as File).type
+    formData.append('userId', userId.toString())
 
     const baseUrl = ENV.FASTAPI_URL
     const urlSuffix =
