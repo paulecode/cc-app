51740e24b422e49273ab82f83fd1a135b8b45920
Paul Ebert <paul.ebert@code.berlin>
2024-10-02 10:23:15 +0200

    delete chartwrapper

--- components/charts/ChartWrapper.tsx
+++
@@ -1,8 +0,0 @@
-export type ChartWrapperProps = {
-  ChartTitle: string
-}
-
-export const ChartWrapper: React.FC<({ children: React.ReactNode, props: ChartWrapperProps })> = ({ children, props }) => {
-  return <div>{ }</div>
-}
-
