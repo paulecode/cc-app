79ed9d968db888a8d3637398d25cfc6d6e838235
Paul Ebert <paul.ebert@code.berlin>
2025-01-20 17:40:55 +0100

    fix return type of getuser

--- actions/auth/helpers/getUser.ts
+++ actions/auth/helpers/getUser.ts
@@ -1,5 +1,5 @@
-import prisma from "@/lib/prisma";
-import { User } from "@prisma/client";
+import prisma from '@/lib/prisma'
+import { User } from '@prisma/client'
 
 /**
  * Gets a user by uesrname
@@ -7,12 +7,12 @@ import { User } from "@prisma/client";
  * @returns {Prisma.User} Prisma User object
  * @throws Throws an error if there is a problem accessing the database or if the username doesn't correspond to any user.
  */
-export default async function getUser(username: string): Promise<User | null> {
-  try {
-    const user = await prisma.user.findFirstOrThrow({ where: { username } });
-    return user;
-  } catch (error) {
-    console.error("Failed to retrieve user: ", error);
-    throw new Error("Error accessing database");
-  }
+export default async function getUser(username: string): Promise<User> {
+    try {
+        const user = await prisma.user.findFirstOrThrow({ where: { username } })
+        return user
+    } catch (error) {
+        console.error('Failed to retrieve user: ', error)
+        throw new Error("Couldn't find the user")
+    }
 }
