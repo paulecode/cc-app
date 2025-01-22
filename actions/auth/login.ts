e7e59bec78c007b84d9921de98f721bf73cb368c
Paul Ebert <paul.ebert@code.berlin>
2025-01-22 21:06:27 +0100

    rename prevState to _

--- actions/auth/login.ts
+++ actions/auth/login.ts
@@ -1,32 +1,32 @@
-"use server";
+'use server'
 
-import { redirect } from "next/navigation";
-import { loginHandler } from "./helpers/loginHandler";
+import { redirect } from 'next/navigation'
+import { loginHandler } from './helpers/loginHandler'
 
-export const loginAction = async (prevState: any, formData: FormData) => {
-  const username = formData.get("username") as string;
-  const password = formData.get("password") as string;
-  let redirectToHome = false;
+export const loginAction = async (_: any, formData: FormData) => {
+    const username = formData.get('username') as string
+    const password = formData.get('password') as string
+    let redirectToHome = false
 
-  if (!username || !password) {
-    return "Username or password missing";
-  }
-
-  try {
-    const loginSuccesful = await loginHandler(username, password);
-
-    if (loginSuccesful) {
-      redirectToHome = true;
-      return "Logging in...";
-    } else {
-      return "Invalid credentials";
+    if (!username || !password) {
+        return 'Username or password missing'
     }
-  } catch (error) {
-    console.error("Login not succesful: ", error);
-    return "Server error";
-  } finally {
-    if (redirectToHome) {
-      redirect("/home");
+
+    try {
+        const loginSuccesful = await loginHandler(username, password)
+
+        if (loginSuccesful) {
+            redirectToHome = true
+            return 'Logging in...'
+        } else {
+            return 'Invalid credentials'
+        }
+    } catch (error) {
+        console.error('Login not succesful: ', error)
+        return 'Server error'
+    } finally {
+        if (redirectToHome) {
+            redirect('/home')
+        }
     }
-  }
-};
+}
