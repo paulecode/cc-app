9183d4bf276fc345f6c3f8a62da22aabe78f32ad
Paul Ebert <paul.ebert@code.berlin>
2025-01-22 15:22:08 +0100

    remove unreachable code

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
 import { ENV } from '@/lib/env'
-import Image from 'next/image'
 import { redirect } from 'next/navigation'
 
 export default function Home() {
     ENV
 
     redirect('/auth')
-
-    return (
-        <main></main>
-    )
 }
